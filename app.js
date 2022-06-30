7a14a4c4ce3f3188256a34704906b3d15e8d0139
Jonnata Costa <jonnatacosta@hotmail.com>
2022-06-30 01:16:31 -0300

    configurando app.js

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'src','views'));
 app.set('view engine', 'ejs');
 
-app.use(express.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
