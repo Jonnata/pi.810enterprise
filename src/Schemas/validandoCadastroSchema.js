f093c00c7419e60d975f6eb665997f82806caba8
Jonnata Costa <jonnatacosta@hotmail.com>
2022-07-27 21:37:52 -0300

    Criando validação de user

--- src/Schemas/validandoCadastroSchema.js
+++ src/Schemas/validandoCadastroSchema.js
@@ -5,18 +5,42 @@ const validandoCadastroSchema = Joi.object({
         .alphanum()
         .min(3)
         .max(30)
-        .required(),
+        .required()
+        .messages({
+            'alphanum': 'O campo username deve conter apenas letras e números',
+            'string.min': 'O campo username deve ter no mínimo 3 caracteres',
+            'string.max': 'O campo username deve ter no máximo 30 caracteres',
+            'any.required': 'O campo username é obrigatório',
+        }),
  
     password: Joi.string()
-        .pattern(/^[a-zA-Z0-9]{3,30}$/),
+        .pattern(new RegExp('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$'))
+        .required()
+        .messages({
+            'string.pattern.base': 'O campo password deve ter no mínimo 8 caracteres, 1 letra maiúscula, 1 letra minúscula, 1 número e 1 caractere especial',
+            'any.required': 'O campo password é obrigatório',
+        }),
  
-    birth_year: Joi.number()
+    nascimento: Joi.number()
         .integer()
         .min(1900)
-        .max(2001),
+        .max(31-12-2002)
+        .required()
+        .messages({
+            'number.integer': 'O campo nascimento deve ser um número inteiro',
+            'number.min': 'O campo nascimento deve ser no mínimo 01/01/1900',
+            'number.max': 'O campo nascimento deve ser no máximo 31/12/2002',
+            'any.required': 'O campo nascimento é obrigatório',
+        }),
  
     email: Joi.string()
         .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+        .required()
+        .messages({
+            'string.email': 'O campo email deve ser um email válido',
+            'string.min': 'O campo email deve ter no mínimo 2 domínios',
+            'any.required': 'O campo email é obrigatório',
+        }),
 })
  
 module.exports = validandoCadastroSchema
--- src/routers/cadastroRouter.js
+++ src/routers/cadastroRouter.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 const cadastroController = require('../controllers/cadastroController')
+const router = express.Router();
 
 router.get('/cadastro', cadastroController.renderCadastro);
+router.post('/cadastro', cadastroController.create)
+
 
 module.exports = router;
