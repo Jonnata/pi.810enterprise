5648d079ec56acc828792eb3471f2fd2b4c45520
Jonnata Costa <jonnatacosta@hotmail.com>
2022-07-29 15:46:01 -0300

    middleware auth

---
+++ src/middlewares/auth.js
@@ -0,0 +1,12 @@
+function auth(req, res, next) {
+    const userIslogged = Boolean(req.session.user) || Boolean(req.user)
+  
+    if(userIslogged) {
+      return next();
+    }
+  
+    return res.send('Você não está logado no sistema')
+  }
+  
+  module.exports = auth;
+  
