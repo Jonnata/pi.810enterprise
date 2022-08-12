88ab090ed189dd0d5122fecf2c88277ef7fcfa8a
Jonnata <jonnatacosta@hotmail.com>
2022-08-11 22:03:03 -0300

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "Ricco@159",
+    "password": "",
     "database": "database_810enterprise",
     "host": "127.0.0.1",
     "dialect": "mysql"
