31ecb55421c0314868f4335bcb542b49da357d5f
Jonnata Costa <jonnatacosta@hotmail.com>
2022-08-31 14:09:11 -0300

    painel usuário...

--- src/database/seeders/20220803215140-users.js
+++ src/database/seeders/20220803215140-users.js
@@ -7,34 +7,25 @@ module.exports = {
         username: 'Jonnata Costa',
         date: '1992-10-15',
         email: 'jonnata@gmail.com',
-        password: '12345678', 
-<<<<<<< HEAD
-=======
+        password: '12345678',
         image: 'public/images/painelUsuario/avatar.png',
         type: 'admin',
->>>>>>> 5f7fc2c771279a1ccf5ee3fd8eaf5cd9c2600933
       },
       { 
         username: 'Maria Marques',
         date: '1995-12-30',
         email: 'maria@gmail.com',
         password: '12345678',
-<<<<<<< HEAD
-=======
         image: 'public/images/painelUsuario/avatar.png',
         type: 'user',
->>>>>>> 5f7fc2c771279a1ccf5ee3fd8eaf5cd9c2600933
       },
       { 
         username: 'Douglas Santos',
         date: '1997-09-20',
         email: 'douglas@gmail.com',
         password: '12345678',
-<<<<<<< HEAD
-=======
         image: 'public/images/painelUsuario/avatar.png',
         type: 'user',
->>>>>>> 5f7fc2c771279a1ccf5ee3fd8eaf5cd9c2600933
       },
     ]);
   },
